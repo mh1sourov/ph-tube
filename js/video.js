2a4956b46066dfd35cd618ffccdd71f4d52ae564
mh1sourov <sourovmh11@gmail.com>
2025-02-12 07:39:23 +0600

    category button and cards added from API

--- js/video.js
+++ js/video.js
@@ -1,1 +1,74 @@
-console.log("connected")
+// loadcategories sec
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch(error => console.log(error))
+}
+
+//display catagories sec
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('category_container');
+    for(let item of categories){
+        
+    const button = document.createElement("button")
+    button.classList = "btn hover:bg-red-600"
+    button.innerText = item.category //`${item.category}`
+    categoryContainer.appendChild(button);
+    
+}
+}
+
+
+const loadVideo = () =>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideo(data))
+    .catch((error) => console.log(error))
+}
+
+const displayVideo = (receiveVideoData) =>{
+const receivedVideo = (receiveVideoData.videos);
+const videoSection = document.getElementById("video");
+
+   for(let video of receivedVideo){
+    console.log(video);
+    console.log("expected", video.title)
+    const card = document.createElement("card")
+    card.innerHTML = `
+    <div class="card card-compact ">
+  <figure class="h-[200px] relative">
+    <img class ="h-full w-full object-cover"
+      src=${video.thumbnail}
+      alt="" />
+    <span class="absolute right-2 bottom-2 bg-black rounded-lg text-gray-300 p-1">${video.others.posted_date}</span>
+  </figure>
+  <div class="px-0 py-3 flex gap-2">
+    <div>
+    <img class='w-[50px] h-[50px] rounded-full' src=${video.authors[0].profile_picture}>
+    </div>
+    <div>
+    <h1 class='font-bold'>${video.title}</h1>
+    <div class='flex gap-1 items-center'>
+    <p class= 'text-gray-500'>${video.authors[0].profile_name}</p>
+    
+    ${video.authors[0].verified ? `<img class='w-5 object-cover' src = 'https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png'>`:''}
+    
+    </div>
+    </div>
+
+  </div>
+</div>
+    `
+    videoSection.appendChild(card)
+   }
+  
+}
+
+loadCategories();
+loadVideo();
+// video id of video section
+
+
